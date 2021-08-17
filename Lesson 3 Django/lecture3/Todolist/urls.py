f2fee5b4baa9b05f77dd86adf1c814879e54f65b
daderuo <davide.ruocco75@gmail.com>
2021-08-17 13:35:24 +0700

    Lesson 3 finisched, need to practice

--- Lesson 3 Django/lecture3/Todolist/urls.py
+++ Lesson 3 Django/lecture3/Todolist/urls.py
@@ -2,9 +2,10 @@ from  django.urls import  path
 
 from . import views
 
-
+app_name = "task"
 #vale l'ordine in cui scrivo le casistiche: i casi generici vanno in fondo, quelli specifici all'inizio
 urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add, name="add"),
+    path("remove/", views.remove, name="remove"),
 ]
--- Lesson 3 Django/lecture3/Todolist/views.py
+++ Lesson 3 Django/lecture3/Todolist/views.py
@@ -1,12 +1,59 @@
+from django import forms
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-Todo = ["wake up", "eat", "sleep"]
+class NewTaskForm(forms.Form):
+    NewTask = forms.CharField(label="Write new task")
+
+class RemoveTaskForm(forms.Form):
+    Remove = forms.CharField(label="Removed")
 
 # Create your views here.
 def index(request):
+    if "Todo" not in request.session:
+        request.session["Todo"] = []
     return render(request, "Todolist/index.html", {
-        "Todo": Todo
+        "Todo": request.session["Todo"]
     })
 
 def add(request):
-    return render(request, "Todolist/add.html")
+    if request.method == "POST":
+        saved_data = NewTaskForm(request.POST)
+
+        if saved_data.is_valid():
+            new = saved_data.cleaned_data["NewTask"]
+            list = request.session["Todo"]
+            list.append(new)
+            request.session["Todo"] = list
+            return HttpResponseRedirect(reverse("task:index"))
+        
+        else:
+            return render(request, "Todolist/add.html", {
+                "form":saved_data
+            })
+
+    return render(request, "Todolist/add.html", {
+        "form" : NewTaskForm()
+    })
+
+def remove(request):
+    if request.method == "POST":
+        data = RemoveTaskForm(request.POST)
+
+        if data.is_valid():
+            new = data.cleaned_data["Remove"]
+            list = request.session["Todo"]
+            list.remove(new)
+            request.session["Todo"] = list
+
+            return HttpResponseRedirect(reverse("task:index"))
+        
+        else:
+            return render(request, "Todolist/remove.html", {
+                "form":data
+            })
+
+    return render(request, "Todolist/remove.html", {
+        "Todo": request.session["Todo"]
+    })
