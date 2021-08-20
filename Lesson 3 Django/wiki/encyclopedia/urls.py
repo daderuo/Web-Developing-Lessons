f0062b58408697db41958b004fa8722cb6320343
daderuo <davide.ruocco75@gmail.com>
2021-08-21 00:13:58 +0700

    Wiki is going on first 3 request done!

--- Lesson 3 Django/wiki/encyclopedia/urls.py
+++ Lesson 3 Django/wiki/encyclopedia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/", views.show, name="Show"),
+    path("wiki/<str:Title>", views.show, name="show")
 ]
--- Lesson 3 Django/wiki/encyclopedia/views.py
+++ Lesson 3 Django/wiki/encyclopedia/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    Entries = util.list_entries()
+    result = []
 
+    if request.method == 'GET':
+        q = str(request.GET.get('q', ''))
+        if q != '':
+            for entry in Entries:
+                if q == entry:
+                    return HttpResponseRedirect(reverse('wiki:show', args=(q,)))
+                if q in entry:
+                    result.append(entry)
+            return render(request, "encyclopedia/index.html", {
+                "entries": result
+            })
+    
+    return render(request, "encyclopedia/index.html", {
+        "entries": Entries
+    })
+    
+
+def show(request, Title = None):
+    Content = util.get_entry(Title)
+    if Content == None:
+        Title = "Error"
+        Content = "Error 404 Page not found!"
+    return render(request, "encyclopedia/show.html", {
+        "Title" : Title,
+        "ToShow" : Content
+    })
