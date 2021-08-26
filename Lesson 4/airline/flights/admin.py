7aad2850ffbdc6a46054ea919701e303d36356c3
daderuo <davide.ruocco75@gmail.com>
2021-08-26 12:20:33 +0700

    Lesson 4 finished need to do exercise

--- Lesson 4/airline/flights/admin.py
+++ Lesson 4/airline/flights/admin.py
@@ -4,8 +4,12 @@ from .models import *
 
 # Register your models here.
 
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-
-admin.site.register(Flight)
-
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
--- Lesson 4/airline/flights/views.py
+++ Lesson 4/airline/flights/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from  django.urls import  reverse
+from django.http import HttpResponse,HttpResponseRedirect
 
-from .models import Flight
-
-from django.http import HttpResponse
+from .models import Flight, Passenger
 
 # Create your views here.
 
@@ -15,9 +15,21 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     passengers = flight.passenger.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html",{
         "flight": flight,
-        "passengers": passengers
+        "passengers": passengers,
+        "non_passengers": non_passengers
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        passenger.flights.add(flight)
+        
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
 
 
